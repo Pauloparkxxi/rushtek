102b0e473af7235c2b333ad9f928b5a71ebd1d86
James Martinez <amajamesmartinez@gmail.com>
2023-01-16 05:25:32 +0800

    Add Username To DB and Seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('fname');
             $table->string('lname');
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('avatar')->nullable();
             $table->integer('role');
