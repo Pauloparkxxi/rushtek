492470124aa35859276afd2710f18eca66cc4131
james.martinez <amajamesmartinez@gmail.com>
2023-01-18 04:32:32 +0800

    Username Implementation: Login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('iunique', function ($attribute, $value, $parameters, $validator) {
+            $query = DB::table($parameters[0]);
+            $column = $query->getGrammar()->wrap($parameters[1]);
+            $login = $query->whereRaw("BINARY {$parameters[1]} = ?", [$value])->count();
+            return $login;
+        },'Error: Record not found');
     }
 }
