540a6c614cdf1e89eae2bf5c13ebe78757be353e
james.martinez <amajamesmartinez@gmail.com>
2023-01-17 19:32:09 +0800

    Username Implementation: Staff

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -95,6 +95,7 @@ class StaffController extends Controller
             'fname'     => 'required|max:255',
             'lname'     => 'required|max:255',
             'email'     => 'required|email|unique:users,email',
+            'username'  => 'required|max:20|min:6',
             'avatar'    => 'max:10000|mimes:jpeg,jpg,png',
             'department' => 'required|max:255',
             'contact'   => 'required|max:255',
@@ -104,6 +105,7 @@ class StaffController extends Controller
         $staff = User::create([
             'fname'     => Str::ucfirst(Str::lower($request->fname)),
             'lname'     => Str::ucfirst(Str::lower($request->lname)),
+            'username'  => $request->username,
             'email'     => $request->email,
             'password'  => Hash::make($request->password),
             'status'    => 1,
@@ -142,6 +144,7 @@ class StaffController extends Controller
             'fname'     => 'required|max:255',
             'lname'     => 'required|max:255',
             'email'     => 'required|email|unique:users,email,'.$id,
+            'username'  => 'required|max:20|min:6',
             'department' => 'required|max:255',
             'contact'   => 'required|max:255',
             'birthdate' => 'required|date',
@@ -155,6 +158,7 @@ class StaffController extends Controller
             'lname' => $request->lname,
             'fname' => $request->fname,
             'email' => $request->email,
+            'username' => $request->username,
             'status' => $request->status,
         ]);
 
